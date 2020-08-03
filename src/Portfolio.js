bef957b87cb330fbb610e96b9ddb7f0107d1f479
seeoya <seeoya@naver.com>
2020-08-03 21:11:05 +0900

    update portfolio map

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -5,59 +5,122 @@ function Portfolio({ selectPage }) {
   const sectionList = [
     {
       id: 1,
-      img: "./img/cinque.jpg",
-      title: "1번 타이틀",
-      date: "2020. 00. 00 ~ 2020. 00. 00",
-      desc:
-        "국가는 농지에 관하여 경자유전의 원칙이 달성될 수 있도록 노력하여야 하며, 농지의 소작제도는 금지된다. 헌법재판소는 법관의 자격을 가진 9인의 재판관으로 구성하며, 재판관은 대통령이 임명한다.",
-      link: "asd"
+      img: ["./img/cinque.jpg"],
+      title: "홈 쉐어링 서비스 Project P5",
+      date: "2020. 02. 24 ~ 2020. 03. 20",
+      set: ["5인, 개발 기획 및 프론트 엔드 담당", "HTML, CSS, Javascript, Jquery", "(+Java, JSP, Ajax)"],
+      desc: [
+        "Home sharing 서비스에서 Host는 부수입을 창출할 수 있으며, 새로운 사람들을 만나고, 그들이 문화를 경험하는 순간을 함께할 수도 있습니다. 독립한 자녀들의 빈자리를 채우는 등, 심적 수입도 얻을 수 있습니다.",
+        "그리고 Guest 역시 평소의 여행보다 더 깊은 문화를 체험할 수 있으며 그로 인한 색다른 여행 경험이 될 수 있습니다.",
+        "P5는 Home sharing을 중개하는 역할의 서비스 웹으로 누구나 Host, Guest가 될 수 있습니다."
+      ],
+      gitLink: "fff",
+      siteLink: "asdd"
     },
     {
-      id: 2,
-      img: "./img/hungarian.jpg",
-      title: "2번 타이틀",
-      date: "2020. 00. 00 ~ 2020. 00. 00",
-      desc: "근로조건의 기준은 인간의 존엄성을 보장하도록 법률로 정한다. 국무총리는 대통령을 보좌하며, 행정에 관하여 대통령의 명을 받아 행정각부를 통할한다.",
-      link: "asdd"
+      id: 1,
+      img: ["./img/cinque.jpg"],
+      title: "홈 쉐어링 서비스 Project P5",
+      date: "2020. 02. 24 ~ 2020. 03. 20",
+      set: ["5인, 개발 기획 및 프론트 엔드 담당", "HTML, CSS, Javascript, Jquery", "(+Java, JSP, Ajax)"],
+      desc: [
+        "Home sharing 서비스에서 Host는 부수입을 창출할 수 있으며, 새로운 사람들을 만나고, 그들이 문화를 경험하는 순간을 함께할 수도 있습니다. 독립한 자녀들의 빈자리를 채우는 등, 심적 수입도 얻을 수 있습니다.",
+        "그리고 Guest 역시 평소의 여행보다 더 깊은 문화를 체험할 수 있으며 그로 인한 색다른 여행 경험이 될 수 있습니다.",
+        "P5는 Home sharing을 중개하는 역할의 서비스 웹으로 누구나 Host, Guest가 될 수 있습니다."
+      ],
+      gitLink: "fff",
+      siteLink: "asdd"
     },
     {
-      id: 3,
-      img: "./img/lobby.jpg",
-      title: "3번 타이틀",
-      date: "2020. 00. 00 ~ 2020. 00. 22",
-      desc:
-        "대통령은 법률안의 일부에 대하여 또는 법률안을 수정하여 재의를 요구할 수 없다. 각급 선거관리위원회는 선거인명부의 작성등 선거사무와 국민투표사무에 관하여 관계 행정기관에 필요한 지시를 할 수 있다.",
-      link: "ㅁㄴㅇ"
+      id: 1,
+      img: ["./img/cinque.jpg"],
+      title: "홈 쉐어링 서비스 Project P5",
+      date: "2020. 02. 24 ~ 2020. 03. 20",
+      set: ["5인, 개발 기획 및 프론트 엔드 담당", "HTML, CSS, Javascript, Jquery", "(+Java, JSP, Ajax)"],
+      desc: [
+        "Home sharing 서비스에서 Host는 부수입을 창출할 수 있으며, 새로운 사람들을 만나고, 그들이 문화를 경험하는 순간을 함께할 수도 있습니다. 독립한 자녀들의 빈자리를 채우는 등, 심적 수입도 얻을 수 있습니다.",
+        "그리고 Guest 역시 평소의 여행보다 더 깊은 문화를 체험할 수 있으며 그로 인한 색다른 여행 경험이 될 수 있습니다.",
+        "P5는 Home sharing을 중개하는 역할의 서비스 웹으로 누구나 Host, Guest가 될 수 있습니다."
+      ],
+      gitLink: "fff",
+      siteLink: "asdd"
     }
   ];
 
   return (
     <div class="portfolio">
       <div className="portfolio__sectionList">
-        {sectionList.map((i) => (
-          <PortfolioSection key={i.id} img={i.img} title={i.title} date={i.date} desc={i.desc} link={i.link} />
-        ))}
-        {sectionList.map((i) => (
-          <PortfolioSection key={i.id} img={i.img} title={i.title} date={i.date} desc={i.desc} link={i.link} />
-        ))}
-        {sectionList.map((i) => (
-          <PortfolioSection key={i.id} img={i.img} title={i.title} date={i.date} desc={i.desc} link={i.link} />
+        {sectionList.reverse().map((i) => (
+          <PortfolioSection key={i.id} img={i.img} title={i.title} date={i.date} set={i.set} desc={i.desc} gitLink={i.gitLink} siteLink={i.siteLink} />
         ))}
       </div>
     </div>
   );
 }
 
-function PortfolioSection({ key, img, title, date, desc, link }) {
+function PortfolioSection({ key, img, title, date, set, desc, gitLink, siteLink }) {
   return (
-    <div className="portfolio__section">
-      <img src={img} alt={title} className="portfolio__sectionSumbnail" />
+    <div className="portfolio__section" key={key}>
+      {setImg(img, title)}
       <div className="portfolio__sectionTitle">{title}</div>
       <div className="portfolio__sectionDate">{date}</div>
-      <div className="portfolio__sectionDescription">{desc}</div>
-      <div className="portfolio__sectionLink">{link}</div>
+      <div className="portfolio__sectionSet">{setSet(set)}</div>
+      <div className="portfolio__sectionDesc">{setDesc(desc)}</div>
+      <div className="portfolio__sectionLink">
+        {setGitLink(gitLink)}
+        {setSiteLink(siteLink)}
+      </div>
     </div>
   );
 }
 
+function setImg(img, title) {
+  var re = [];
+  for (let index = 0; index < img.length; index++) {
+    re.push(<img src={img[index]} alt={title} className="portfolio__sectionSumbnail" />);
+  }
+  return re;
+}
+
+function setSet(set) {
+  var re = [];
+  for (let index = 0; index < set.length; index++) {
+    if (index == 0) {
+      re.push(<p>{set[index]}</p>);
+    } else {
+      re.push(<p>{set[index]}</p>);
+    }
+  }
+  return re;
+}
+
+function setDesc(desc) {
+  var re = [];
+  for (let index = 0; index < desc.length; index++) {
+    re.push(<p>{desc[index]}</p>);
+  }
+  return re;
+}
+
+function setGitLink(gitLink) {
+  if (gitLink != null && gitLink != "") {
+    return (
+      <a href={gitLink}>
+        <i class="fab fa-github-square fa-2x"></i>
+      </a>
+    );
+  }
+  return;
+}
+
+function setSiteLink(siteLink) {
+  if (siteLink != null && siteLink != "") {
+    return (
+      <a href={siteLink}>
+        <i class="fas fa-share-square fa-2x"></i>
+      </a>
+    );
+  }
+  return;
+}
 export default Portfolio;
